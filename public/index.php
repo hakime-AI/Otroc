22c0c1a607f93035e230a1d4196bd252e53c7a28
hakime-AI <hakimeattoumani.simplon@gmail.com>
2022-02-21 10:23:55 +0100

    add formulaire

--- public/index.php
+++ public/index.php
@@ -10,12 +10,12 @@ require ROOT."/altorouter/AltoRouter.php";
 
 // premier problem mon projet ne fonction pas sans ces require 
 require_once ROOT."/src/Controllers/Controller.php";
-require_once ROOT."/src/Controllers/AnnoncesController.php";
-require_once ROOT."/src/Controllers/AjoutController.php";
-require_once ROOT."/src/Controllers/ShowController.php";
+// require_once ROOT."/src/Controllers/AnnoncesController.php";
+// require_once ROOT."/src/Controllers/AjoutController.php";
+// require_once ROOT."/src/Controllers/ShowController.php";
 //require_once ROOT."/src/Controllers/DeleteController.php";
-require_once ROOT."/src/Controllers/UpdateController.php";
-require_once ROOT."/src/Controllers/ErrorController.php";
+// require_once ROOT."/src/Controllers/UpdateController.php";
+// require_once ROOT."/src/Controllers/ErrorController.php";
 require_once ROOT."/src/Core/Main.php";
 require_once ROOT."/src/Core/Db.php";
 require_once ROOT."/src/Models/Model.php";
@@ -30,21 +30,21 @@ $router = new AltoRouter();
 // $router->map( 'GET', '/Otroc', function() {
 // 	require __DIR__ . '/views/acceuil.php';
 // });
-$router->map( 'GET|POST', '/Otroc/','annonces','annonces');// GET,URL,ADDRESSE,NOM
-$router->map( 'GET|POST', '/Otroc/ajout','ajout','ajout');
+$router->map('GET|POST', '/Otroc/','annonces','annonces');// GET,URL,ADDRESSE,NOM
+$router->map('GET|POST', '/Otroc/ajout','ajout','ajout');
 $router->map('GET|POST', '/Otroc/update', 'update', 'update');
 $router->map('GET|POST','/Otroc/delete','delete', 'delete');
 $router->map('GET|POST', '/Otroc/show', 'show', 'show');
-$router->map( 'GET|POST', '/Otroc/error','error','error');
-$router->map( 'GET|POST', '/Otroc/validation','validation','validation');
+$router->map('GET|POST', '/Otroc/error','error','error');
+$router->map('GET|POST', '/Otroc/validation','validation','validation');
 
 // $router->map( 'GET|POST', '/Otroc/[*:slug]','annonce','annoncePerso');
-$router->map( 'GET|POST', '/Otroc/ajout/[*:slug]','ajout','ajoutPerso');
+$router->map('GET|POST', '/Otroc/ajout/[*:slug]','ajout','ajoutPerso');
 $router->map('GET|POST', '/Otroc/update/[*:slug]', 'update', 'updatePerso');
 $router->map('GET|POST', '/Otroc/delete/[*:slug]', 'delete', 'deletePerso');
 $router->map('GET|POST', '/Otroc/show/[*:slug]', 'show', 'showPerso');
-$router->map( 'GET|POST', '/Otroc/error/[*:slug]','error');
-$router->map( 'GET|POST', '/Otroc/validation/[*:slug]','validation','validationPerso');
+$router->map('GET|POST', '/Otroc/error/[*:slug]','error');
+$router->map('GET|POST', '/Otroc/validation/[*:slug]','validation','validationPerso');
 
 $match = $router->match();
 $app->router($match);
