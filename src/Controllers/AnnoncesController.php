f8db77f83e3f43c8ebb9f928f37344a938556602
hakime-AI <hakimeattoumani.simplon@gmail.com>
2022-02-22 13:06:54 +0100

    clear comment

--- src/Controllers/AnnoncesController.php
+++ src/Controllers/AnnoncesController.php
@@ -5,11 +5,7 @@ class AnnoncesController extends Controller{
     public function index(){
         $annoncesModel = new AnnoncesModel;
         $annonces = $annoncesModel->findAll();
-        // $annoncesModel->setnom($annonces[0]['nom']);
         
         $this->twig->display('annonces.html.twig', compact("annonces"));
-        // $newnom=$annoncesModel->getnom();
-        // echo "<pre>",$newnom,"</pre>";
-        
     }
 }
