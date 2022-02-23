0207fd863816351ad2160184ab25d0aaea0854a0
codeuronline <j.kasperski@codeur.online>
2022-02-23 10:10:37 +0100

    front

--- src/Controllers/AnnoncesController.php
+++ src/Controllers/AnnoncesController.php
@@ -9,15 +9,25 @@ class AnnoncesController extends Controller{
         $annoncesModel = new AnnoncesModel;
         $annonces = $annoncesModel->findAll();
         $PhotoModel = new PhotoModel;
+        $EmailModel = new EmailModel;
         
         for($i=0;$i<count($annonces);$i++){
             $photos[$i] = $PhotoModel->find('photo','id_annonce',$annonces[$i]['id']);
             if(isset($photos[$i]['photo'])){
                 $annonces[$i]['photo1']=$photos[$i]['photo'];
             }
+            if(isset($annonces[$i]['id_email'])){
+            $email[$i] = $EmailModel->find('email','id',$annonces[$i]['id_email']);
+            if(isset($email[$i]['email'])){
+            $annonces[$i]['email']=$email[$i]['email'];
+            }
+            }
+            // if(isset($photos[$i]['photo'])){
+            //     $annonces[$i]['photo1']=$photos[$i]['photo'];
+            // }
         }
         
-        echo "<pre>",print_r($annonces),"</pre>";
+        //echo "<pre>",print_r($annonces),"</pre>";
         $this->twig->display('annonces.html.twig', compact("annonces"));
     }
 }
