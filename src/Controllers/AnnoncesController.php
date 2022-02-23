dae2b32bbbe3753bb8cbe5121aa1aaf9dfd4acde
codeuronline <j.kasperski@codeur.online>
2022-02-23 09:46:02 +0100

    front annonce update

--- src/Controllers/AnnoncesController.php
+++ src/Controllers/AnnoncesController.php
@@ -1,13 +1,23 @@
 <?php 
 namespace App\Controllers;
 use App\Models\AnnoncesModel;
+use App\Models\PhotoModel;
+use App\Models\EmailModel;
 
 class AnnoncesController extends Controller{
     public function index(){
         $annoncesModel = new AnnoncesModel;
         $annonces = $annoncesModel->findAll();
+        $PhotoModel = new PhotoModel;
         
+        for($i=0;$i<count($annonces);$i++){
+            $photos[$i] = $PhotoModel->find('photo','id_annonce',$annonces[$i]['id']);
+            if(isset($photos[$i]['photo'])){
+                $annonces[$i]['photo1']=$photos[$i]['photo'];
+            }
+        }
         
+        echo "<pre>",print_r($annonces),"</pre>";
         $this->twig->display('annonces.html.twig', compact("annonces"));
     }
 }
