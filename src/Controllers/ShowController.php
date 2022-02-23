224030b3217e075d43c919d16b957129b6969de9
hakime-AI <hakimeattoumani.simplon@gmail.com>
2022-02-23 10:42:44 +0100

    maj show

--- src/Controllers/ShowController.php
+++ src/Controllers/ShowController.php
@@ -5,17 +5,21 @@ use App\Models\PhotoModel;
 use App\Models\EmailModel;
 class ShowController extends Controller{
     public function index(){
+
         $AnnoncesModel = new AnnoncesModel;
+        $PhotoModel = new PhotoModel;
+        $EmailModel = new EmailModel;
+
         $initAnnonce = $AnnoncesModel->findBy(['id'=>$_SESSION['param']['slug']]);
         $annonce = $initAnnonce[0];
-        $PhotoModel = new PhotoModel;
         $photos = $PhotoModel->findBy(['id_annonce'=>$_SESSION['param']['slug']]);
-        $EmailModel = new EmailModel;
         $email = $EmailModel->findBy(['id'=>$annonce['id_email']]);
-        $result =array_merge($annonce,$photos,$email);
+        for($i=0;$i<5;$i++){
+            $annonce['photo'.$i]=$photos[$i]['photo'];
+        }
+        $annonce['email']=$email[0]['email'];
         
-        // echo "<pre>",print_r($result),"</pre>";
-        $this->twig->display('show.html.twig',compact("result"));
-        
+        // echo "<pre>",print_r($annonce),"</pre>";
+        $this->twig->display('show.html.twig',compact("annonce"));
     }
 }
